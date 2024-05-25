8245d26b7a1a5ef0987e4f37647b4704c3891f60
Chetan <sonigrachetan44@gmail.com>
2024-05-26 05:29:04 +0530

    Modifies project.py withsome model configuration

--- project.py
+++ project.py
@@ -1,10 +1,24 @@
-from pydantic import BaseModel,ValidationError
+from pydantic import BaseModel,ValidationError, ConfigDict
 from datetime import date
+from enum import Enum
+
+class AutomobileType(Enum):
+    sedan = 'Sedan'
+    coupe = 'Coupe'
+    convertible = 'Convertible'
+    suv = 'SUV'
+    truck = 'Truck'
+
 
 class Automobile(BaseModel):
+    model_config = ConfigDict(extra='forbid',
+                              str_strip_whitespace=True,
+                              validate_default=True,
+                              validate_assignment=True)
+
     manufacturer: str
     series_name: str
-    type: str
+    type_: AutomobileType
     is_electric: bool = False
     manufactured_date: date
     base_msrp_usd: float
