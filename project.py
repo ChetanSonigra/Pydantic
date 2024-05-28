b41847a53ff503ff77396136c8848aa8b9ce9008
Chetan <sonigrachetan44@gmail.com>
2024-05-28 17:57:49 +0530

    Modifies project.py to add registration date validator and country validator.

--- project.py
+++ project.py
@@ -1,15 +1,26 @@
-from pydantic import BaseModel,ValidationError, ConfigDict, Field,field_serializer, UUID4
+from pydantic import BaseModel,ValidationError, ConfigDict, \
+                        Field,field_serializer, UUID4, field_validator, ValidationInfo, AfterValidator
 from pydantic.alias_generators import to_camel
 from datetime import date
 from uuid import uuid4
 from enum import Enum
 from typing import Annotated, TypeVar
-
+from country_data import countries
 
 T = TypeVar('T')
 BoundedString = Annotated[str,Field(min_length=2, max_length=50)]
 BoundedList = Annotated[list[T],Field(min_length=1, max_length=5)]
+valid_countries = [x for x in countries]
 
+def lookup_country(name: str) -> tuple[str,str]:
+    try:
+        return countries[name]
+    except KeyError:
+        raise ValueError("Unknown country. "
+                         f"Country name must be one of {",".join(valid_countries)}"
+                        )
+        
+Country = Annotated[BoundedString,AfterValidator(lambda name:lookup_country(name)[0])]
 
 class AutomobileType(Enum):
     sedan = 'Sedan'
@@ -36,13 +47,24 @@ class Automobile(BaseModel):
     top_features: BoundedList[BoundedString] | None = None
     vin: BoundedString
     number_of_doors: int = Field(validation_alias="doors",default=4,ge=2,le=4,multiple_of=2)
-    registration_country: BoundedString | None = None
+    registration_country: Country | None = None
+    registration_date: date | None = None
     license_plate: BoundedString | None = None
 
-    @field_serializer("manufactured_date",when_used='json-unless-none',)
+    @field_serializer("manufactured_date","registration_date",when_used='json-unless-none',)
     def manufactured_date_serializer(self,value):
         value = value.strftime("%Y/%m/%d")
         return value
+    
+    @field_validator('registration_date')
+    @classmethod
+    def registration_date_validator(cls,dt:date,info: ValidationInfo):
+        data = info.data
+        if 'manufactured_date' in data:
+            if dt<data['manufactured_date']:
+                raise ValueError("Registration date can not be prior to manufactured date.")
+        return dt
+    
 
 data = {
     "id": "c4e60f4a-3c7f-4da5-9b3f-07aee50b23e7",
@@ -55,14 +77,21 @@ data = {
     "topFeatures": ["6 cylinders", "all-wheel drive", "convertible"],
     "vin": "1234567890",
     "doors": 2,
-    "registrationCountry": "France",
+    "registrationCountry": "usa",
+    "registrationDate": "2023-01-01",
     "licensePlate": "AAA-BBB"
 }
 
 
-m = Automobile.model_validate(data)
+try:
+    m = Automobile.model_validate(data)
+except ValidationError as ex:
+    exception = ex.json(indent=2)
 
-print(m.model_dump())
-print(m.model_dump(by_alias=True))
-print(m.model_dump_json())
+
+print(m)
+
+# print(m.model_dump())
+# print(m.model_dump(by_alias=True))
+# print(m.model_dump_json())
 print(m.model_dump_json(by_alias=True))
